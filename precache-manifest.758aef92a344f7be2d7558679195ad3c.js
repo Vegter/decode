7e434b8e3584b5cdd78406893b2921784a5e9422
vegter <amsjurien@gmail.com>
2018-12-12 17:01:46 +0100

    deploy

---
+++ precache-manifest.758aef92a344f7be2d7558679195ad3c.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f7ebf29cd3826875a9c",
+    "url": "/js/readQR.8d840323.js"
+  },
+  {
+    "revision": "47e0f448ea5352abe3f5",
+    "url": "/js/about.73892acb.js"
+  },
+  {
+    "revision": "daf13f4f37e3b7b2f40b",
+    "url": "/js/app.7a4bb07f.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "6cc620c1ce177bead494",
+    "url": "/js/chunk-vendors.e3953c72.js"
+  },
+  {
+    "revision": "2d5c5698ec5b5ad30ecc",
+    "url": "/js/zenroom.7f47111a.js"
+  },
+  {
+    "revision": "7b7a205b51d276717ca0",
+    "url": "/js/disclosure.4b474a22.js"
+  },
+  {
+    "revision": "abbf6dcc6f34aeced071",
+    "url": "/js/disclosure~onboarding~question~readQR~scan~showQR.619d00e3.js"
+  },
+  {
+    "revision": "f2f2b6600d0e8fc3d8d9",
+    "url": "/js/showQR.3187ea48.js"
+  },
+  {
+    "revision": "836f7dddd48f2bd47b03",
+    "url": "/js/onboarding.fca42c87.js"
+  },
+  {
+    "revision": "097420ee3cc9f4ac5841",
+    "url": "/js/scan.51c4e01b.js"
+  },
+  {
+    "revision": "6fe46539e9d7a353462a",
+    "url": "/js/profile.f3228f47.js"
+  },
+  {
+    "revision": "a4044ded5ffa5ce6daa7",
+    "url": "/js/question~showQR.10c0caec.js"
+  },
+  {
+    "revision": "25dd617a32a1ace74e1d",
+    "url": "/js/question.d8b5f048.js"
+  },
+  {
+    "revision": "7edb4af0b96401f9d568d684aaacf136",
+    "url": "/img/decode-logo.7edb4af0.png"
+  },
+  {
+    "revision": "b00444be7ee8f676fd14d50aeb8321ac",
+    "url": "/index.wasm"
+  },
+  {
+    "revision": "60213ba4d54b0668f28fcbeb32a259a6",
+    "url": "/img/animated-logo.60213ba4.gif"
+  },
+  {
+    "revision": "0f7ebf29cd3826875a9c",
+    "url": "/css/readQR.83a7c12f.css"
+  },
+  {
+    "revision": "097420ee3cc9f4ac5841",
+    "url": "/css/scan.13cb0848.css"
+  },
+  {
+    "revision": "33092993c92dcef7b013cd8052d9719f",
+    "url": "/index-new.wasm"
+  },
+  {
+    "revision": "f2f2b6600d0e8fc3d8d9",
+    "url": "/css/showQR.14503b6a.css"
+  },
+  {
+    "revision": "d1a3d1609008da390722aae86a4543eb",
+    "url": "/index.js"
+  },
+  {
+    "revision": "2d5c5698ec5b5ad30ecc",
+    "url": "/css/zenroom.75b96ecb.css"
+  },
+  {
+    "revision": "0e7a7a2d9e32e21bc675bd94b5757d57",
+    "url": "/index.data"
+  },
+  {
+    "revision": "6542ce8845f1c910203c0ab68c4f7e1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4044ded5ffa5ce6daa7",
+    "url": "/css/question~showQR.e961fb36.css"
+  },
+  {
+    "revision": "25dd617a32a1ace74e1d",
+    "url": "/css/question.41ee7d79.css"
+  },
+  {
+    "revision": "6fe46539e9d7a353462a",
+    "url": "/css/profile.24c0b067.css"
+  },
+  {
+    "revision": "836f7dddd48f2bd47b03",
+    "url": "/css/onboarding.227be00a.css"
+  },
+  {
+    "revision": "7b7a205b51d276717ca0",
+    "url": "/css/disclosure.5ca09c4d.css"
+  },
+  {
+    "revision": "6cc620c1ce177bead494",
+    "url": "/css/chunk-vendors.35fed924.css"
+  },
+  {
+    "revision": "daf13f4f37e3b7b2f40b",
+    "url": "/css/app.3e35c75c.css"
+  },
+  {
+    "revision": "a8bb91baccfaed95d751e29fb0c61376",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "e71cbdb89e55244fd6ae2e7d109dec68",
+    "url": "/apple-touch-icon.png"
+  },
+  {
+    "revision": "ad171a32a2b5bcdf4eb6213b80b4353d",
+    "url": "/404.html"
+  }
+];
