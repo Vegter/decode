40daba2d99b3c132ccb973b1f561c948e6ccd820
Mark van der Net <mark@oscity.nl>
2018-12-12 17:50:58 +0100

    started with design

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
+  <div id="profile-view-content">
     <profile :base="base"></profile>
-    <button class="button is-link" @click="openQrScanner()">Scan QR</button>
+    <div id="verified-by"><img src="../assets/verified_by_amsterdam.svg"/></div>
+    <a class="button-verify-identity" @click="openQrScanner()"><img src="../assets/qr_scan.svg"/>Verify identity</a>
   </div>
 </template>
 
@@ -12,7 +13,7 @@ import Profile from "../components/Profile";
 export default {
   data() {
     return {
-      picture: "../assets/logo.png",
+      picture: "./assets/stan.jpg", // https://github.com/vuejs/Discussion/issues/202
       surname: "Jansen",
       firstname: "Jan",
       dateOfBirth: "01 01 1990",
@@ -38,3 +39,41 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+
+#profile-view-content {
+  border: 1px solid black;
+}
+
+#verified-by {
+  
+}
+
+#verified-by img {
+  display: block;
+  height: 10vh;
+  width: auto;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+a.button-verify-identity {
+  display: block;
+  background: #c60039;
+  width: 80vw;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 6vh;
+  height: 7vh;
+  color: white;
+  text-align: center;
+  padding-top: 2vh;
+}
+
+a.button-verify-identity img {
+  vertical-align: top;
+  margin-right: 20px;
+}
+
+</style>
